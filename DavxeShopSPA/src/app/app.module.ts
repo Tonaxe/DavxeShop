5e5ab6e9afe1ecbd9cd602e440eda177df5ef955
Tonaxe <ymennana@elpuig.xeill.net>
2025-03-28 12:45:05 +0100

    V006: Created login component with form and register component.

--- DavxeShopSPA/src/app/app.module.ts
+++ DavxeShopSPA/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms'; 
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
---
+++ DavxeShopSPA/src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado:', this.loginForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
+}
