1d671f4826a660bb9bd798a041bb8b1047b36f45
DaVinci25 <said25022004@gmail.com>
2025-04-01 23:31:19 +0200

    V026: Home designe(productos).

--- DavxeShopSPA/src/app/home/home.component.ts
+++ DavxeShopSPA/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   standalone: false,
@@ -11,4 +11,20 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   } 
+  @ViewChild('contenedor', { static: false }) contenedor!: ElementRef;
+  productos = [
+    'Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto 5',
+    'Producto 6', 'Producto 7', 'Producto 8', 'Producto 9', 'Producto 10'
+  ];
+
+  scrollLeft() {
+    this.contenedor.nativeElement.scrollTo({ left: 0, behavior: 'smooth' });
+  }
+  
+  scrollRight() {
+    const maxScroll = this.contenedor.nativeElement.scrollWidth - this.contenedor.nativeElement.clientWidth;
+    this.contenedor.nativeElement.scrollTo({ left: maxScroll, behavior: 'smooth' });
+  }
+  
+  
 }
