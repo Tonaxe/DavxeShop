7a159a7ab852451bb6c0c502e7c4668a71992d8d
Tonaxe <ymennana@elpuig.xeill.net>
2025-05-22 18:58:46 +0200

    V066: Reedirect to product details from slider.

--- DavxeShopSPA/src/app/shared/slider/slider.component.ts
+++ DavxeShopSPA/src/app/shared/slider/slider.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Producto } from '../../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
@@ -9,4 +10,10 @@ import { Producto } from '../../models/product.model';
 })
 export class SliderComponent {
   @Input() productos: Producto[] = [];
+
+  constructor(private router: Router) {}
+
+  irDetalle(productoId: number) {
+    this.router.navigate(['/detalle', productoId]);
+  }
 }
